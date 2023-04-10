8cf8d640a8bbf219dd86c8aea3430f7ede2a0127
Kaushal Bhardwaj <kaushalbhardwaj1125@gmail.com>
2023-04-10 18:42:54 +0530

    Update deploy.js

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,12 +1,11 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const compiledFactory = require("./build/CampaignFactory.json");
-const provider = new HDWalletProvider("hope person horn nerve attend exchange congress raise eagle grape size during","https://goerli.infura.io/v3/65e209fc3ac3488fa1b2daf90798a0be")
+const provider = new HDWalletProvider("<YOUR_PNEUMONIC>","<YOUR_NODE_URL>")
 const web3 = new Web3(provider);
 
 
-// https://sepolia.infura.io/v3/3988ce8237ca4c1898f6e68b751e6de2
-//https://goerli.infura.io/v3/65e209fc3ac3488fa1b2daf90798a0be
+
 
 let accounts;
 
